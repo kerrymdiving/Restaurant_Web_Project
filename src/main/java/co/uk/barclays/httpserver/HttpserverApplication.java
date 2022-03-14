01cbe741e8e590e6ed625b10e7e86fbf2460f39f
kerrymdiving <kerry.murphy@mac.com>
2022-03-14 19:20:52 +0000

    initial commit

---
+++ src/main/java/co/uk/barclays/httpserver/HttpserverApplication.java
@@ -0,0 +1,13 @@
+package co.uk.barclays.httpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpserverApplication.class, args);
+	}
+
+}
